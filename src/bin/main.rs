d1b6a20533696b241860b901a501471794bf17f9
Jeremy <jeremydezmond@gmail.com>
2017-07-01 06:21:19 -0700

    functional. adding serde now

---
+++ src/bin/main.rs
@@ -0,0 +1,33 @@
+extern crate memo;
+
+use std::io;
+use memo::MemoBox;
+
+fn main() {
+    println!("Please enter nth fibonacci position to print");
+    let mut n = String::new();
+    io::stdin().read_line(&mut n).expect("read_line failed");
+    let n: u32 = match n.trim().parse() {
+        Ok(num) => num,
+        Err(_)  => {
+            println!("You did not enter a number, so you get the 7th position");
+            7
+        },
+    };
+
+    let mut callbox = MemoBox::new(find_fib);
+    let result = callbox.call(n);
+    println!("The {}th number is: {}", n, result);
+    println!("Attempting resolve...");
+    let result = callbox.call(n);
+    println!("The {}th number is: {}", n, result);
+}
+
+fn find_fib(n: u32) -> u32 {
+    match n {
+        0 | 1 => n,
+        _     => {
+            find_fib(n-1)+find_fib(n-2)
+        },
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,79 @@
+/* 
+ * Simple memoization library
+ */
+
+//! Call function through memoization wrapper
+//! Memoization wrapper will search data structure
+//! for previous cached calls and return stored
+//! value if found.
+
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate bincode;
+
+//TODO: Add serialization and deserialization, serde
+
+use std::hash::Hash;
+use std::collections::HashMap;
+use std::fmt::Display;
+use serde::ser::Serialize;
+use serde::de::Deserialize;
+use bincode::{serialize, deserialize, Infinite};
+
+//impl<K, V> Serialize for HashMap<K, V>
+//    where K: Serialize,
+//          V: Serialize
+//{
+//    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+//        where S: Serializer
+//    {
+//        let mut map = serializer.serialize_map(Some(self.len()))?;
+//        for (k, v) in self {
+//            map.serialize_entry(k, v)?;
+//        }
+//        map.end()
+//    }
+//}
+//pub struct MemoBox<I: Hash + Eq + Clone + Display, O: Clone + Display> {
+pub struct MemoBox<I, O> {
+    data: HashMap<I, O>,
+    function: fn(I) -> O,
+}
+impl <'a, I: Hash + Eq + Clone + Display, O: Clone + Display> MemoBox<I, O> where
+    I: Serialize + Deserialize<'a>,
+    O: Serialize + Deserialize<'a> {
+    pub fn new(callable: fn(I) -> O) -> Self {
+        MemoBox {
+            data: HashMap::new(),
+            function: callable,
+        }
+    }
+    pub fn call(&mut self, input: I) -> O {
+        let fun = self.function;
+        let output =
+            self.data.entry(input.clone())
+            .or_insert_with(|| (fun)(input));
+        output.clone()
+    }
+    pub fn des(&self, encoded: Vec<u8>) {
+        let decoded: HashMap<I, O> = deserialize(&encoded).unwrap();
+    }
+    pub fn ser(&self) {
+        let encoded: Vec<u8> = serialize(&self.data, Infinite).unwrap();
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
+        fn square(x: i32) -> i32 { x*x }
+        let mut callbox = CallBox::new(square);
+        let result = callbox.call(5);
+        assert_eq!(result, 25);
+        callbox.dump_table();
     }
 }
